cf7c0adceafd94b74f827802ec3c9f42598fd7ba
Miro Lacoste <kl3in3r.noo@gmail.com>
2026-04-30 13:33:22 +0200

    Initial commit: Projekt setup mit Hellow world

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("willkommen bei Wordle(Clone lol)")
+        System.out.println("Errate das 5-Buchstaben Wort in 6 Versuchen um zu Gewinnen")
+    }
+}
