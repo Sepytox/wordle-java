beb67a5f47649948bb34c5c48a6cc3583ba0b209
Miro Lacoste <kl3in3r.noo@gmail.com>
2026-04-30 13:41:10 +0200

    feat: Wortliste hinzugefügt und zufälliges Wort ausgewählt

--- main.java
+++ main.java
@@ -1,6 +1,25 @@
-public class main {
+import java.util.Random;
+
+public class Main {
+
+    // Eine kleine Auswahl an deutschen Wörtern mit 5 Buchstaben
+    static String[] woerter = {
+        "APFEL", "BLUME", "BREIT", "DANKE", "DELTA",
+        "ENGEL", "FALKE", "GABEL", "HAFEN", "INSEL",
+        "JACKE", "KATZE", "LAMPE", "MAUER", "NACHT",
+        "OFFEN", "PFEIL", "QUARZ", "REGEN", "STERN",
+        "TISCH", "UHREN", "VOGEL", "WELLE", "XENON"
+    };
+
     public static void main(String[] args) {
-        System.out.println("willkommen bei Wordle(Clone lol)")
-        System.out.println("Errate das 5-Buchstaben Wort in 6 Versuchen um zu Gewinnen")
+        // Zufälliges Wort auswählen
+        Random zufall = new Random();
+        String geheimwort = woerter[zufall.nextInt(woerter.length)];
+
+        System.out.println("Willkommen bei Wordle!");
+        System.out.println("Rate das 5-Buchstaben-Wort in 6 Versuchen.");
+
+        // Nur zum Testen – später wieder entfernen!
+        System.out.println("(Debug) Geheimwort: " + geheimwort);
     }
 }
